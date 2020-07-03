270a063dfce84600bbc766a41f5fbc5a5b7a0672
yesmin <ymslahoud@gmail.com>
2020-07-02 22:43:27 -0300

    desafio-primeiro-projeto-node

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,34 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    // não permitirá que uma transação do tipo outcome extrapole
+    // o valor total que o usuário tem em caixa
+    const totalIncome = this.transactionsRepository.reduce(
+      (total: number, transaction: Transaction) => {
+        if (transaction.type === 'income') return total + transaction.value;
+        return total;
+      },
+      0,
+    );
+
+    const totalOutcome = this.transactionsRepository.reduce(
+      (total: number, transaction: Transaction) => {
+        if (transaction.type === 'outcome') return total + transaction.value;
+      },
+      0,
+    );
+    if (totalOutcome > totalIncome) {
+      throw Error(`The outcome transaction doesn't have a valid balance`);
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
